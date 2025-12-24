1509d360559efecb3d861a72706d9787bda83aa4
sai <sai@gmail.com>
2025-12-24 09:45:13 +0530

    Reapply "multiple files"
    This reverts commit 974d105d5b902c56d6bb7c02d83bd653756fbf8c.

--- sample.java
+++ sample.java
@@ -1,9 +1,9 @@
-java code
-  java code
-  java code
-java code
-  java code
-  java code
-  java code
-  java code
-  java code
+java code1
+  java code1
+  java code1
+java code1
+  java code1
+  java code1
+  java code1
+  java code1
+  java code1
